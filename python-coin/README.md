03fae2c690c672a29c89daf6a2e61128d177c292
bion0625 <bion0625@gmail.com>
2024-05-10 13:57:42 +0900

    smtm(show me the money) init

---
+++ python-coin/README.md
@@ -0,0 +1,3 @@
+# Show me the money - hello-python
+
+python 연습을 위한 branch
---
+++ python-coin/smtm/__init__.py
@@ -0,0 +1,9 @@
+"""
+Description for Package
+"""
+from .base import *
+
+__all__ = [
+    'Base',
+]
+__version__ = '0.1.0'
